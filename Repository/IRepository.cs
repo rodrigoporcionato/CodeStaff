c9b3f73660a2ea853c02ff6dc88732e60e4e7fa1
rodrigoporcionato <rodrigoporcionato@gmail.com>
2014-07-18 15:38:44 -0300

    Addd new c# generic repository - repositório generico em C#
    Add generic repository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Utils.Repository
+{
+    public interface IRepository<TEntity>
+    {
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        IQueryable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate);
+
+        IQueryable<TEntity> GetAll();
+
+        Task EditAsync(TEntity entity);
+
+        Task InsertAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Utils.Repository
+{    
+        public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+        {
+            protected DbSet<TEntity> DbSet;
+
+            private readonly DbContext _dbContext;
+
+            public Repository(DbContext dbContext)
+            {
+                _dbContext = dbContext;
+                DbSet = _dbContext.Set<TEntity>();
+            }
+
+            public Repository()
+            {
+            }
+
+            public IQueryable<TEntity> GetAll()
+            {
+                return DbSet;
+            }
+
+            public async Task<TEntity> GetByIdAsync(int id)
+            {
+                return await DbSet.FindAsync(id);
+            }
+
+            public IQueryable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate)
+            {
+                return DbSet.Where(predicate);
+            }
+
+            public async Task EditAsync(TEntity entity)
+            {
+                _dbContext.Entry(entity).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+            }
+
+            public async Task InsertAsync(TEntity entity)
+            {
+
+                DbSet.Add(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            public async Task DeleteAsync(TEntity entity)
+            {
+                DbSet.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+}
